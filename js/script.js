5ad210a40d8d7f599fea15bd25b8c4b565c2a8ca
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-01-05 22:13:35 +0200

    05.01.22

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@
 $(document).ready(function () {
   $("#slides").superslides({
     animation: "fade",
-    play: 5000,
+    play: 7000,
     //pagination: false
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Web Developer.", "Student."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
   });
 });
