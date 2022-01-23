5fb830874d925a778c27f5efb2e26222831c4958
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-01-24 00:01:49 +0200

    23.01.22

--- js/script.js
+++ js/script.js
@@ -79,6 +79,28 @@ $(document).ready(function () {
     return false;
   });
 
+  $("#navigation li a").click(function (e) {
+    e.preventDefault();
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement);
+  });
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+  function stickyNavigation() {
+    var body = $("body");
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
+
   fancybox.bind('[data-fancybox="gallery"]', {
     caption: function (fancybox, carousel, slide) {
       return (
